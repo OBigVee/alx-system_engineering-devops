8b536d31ba6572b9cd1aad5e9d15aeeea2efbb47
OBigVee <adedejivictor98@gmail.com>
2022-05-24 14:53:21 +0100

    Create file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,1 +1,15 @@
+#creates a file with the following resource declaration
+# File path is /tmp/school
+# File permission is 0744
+# File owner is www-data
+# File group is www-data
+# File contains I love Puppet
 
+file { 'tmp/school':
+  ensure => file,
+  path   => '/tmp/school',
+  cotent => 'I love Puppet',
+  owner  => 'www-data',
+  group  => 'www-data',
+  mode   => '0744',
+}
