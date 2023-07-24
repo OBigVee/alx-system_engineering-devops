91678b0940ef30dab2e97c3bcac3d1b8ae98e0ad
OBigVee <adedejivictor98@gmail.com>
2023-07-24 22:16:39 +0100

    task 0 done :heavy_check_mark:

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+import requests
+import sys
+import json
+
+if __name__ == "__main__":
+    employee_id = sys.argv[1]
+    url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    user = requests.get(url)
+    employee_name = user.json().get("name")
+    todos = requests.get("https://jsonplaceholder.typicode.com/todos")
+
+    n_task_done = 0
+    total_number_of_tasks = 0
+
+    for job_done in todos.json():
+        if job_done.get("userId") == int(employee_id):
+            total_number_of_tasks += 1
+            if job_done.get("completed"):
+                n_task_done += 1
+    print("Employee {} is done with task({}/{})".format(
+        employee_name,
+        n_task_done,
+        total_number_of_tasks
+        ))
+    # print(
+    # f"Employee {
+    # employee_name
+    # }is done with task({n_task_done}/{total_number_of_tasks})")
+
+    for job_done in todos.json():
+        if (
+            job_done.get("userId") == int(employee_id)
+            and job_done.get("completed")
+        ):
+            print("".join("\t" + job_done.get("title")))
+        else:
+            "No oo!"
