c6e0888e538abd2dbd2c2c37aac1a2de24b311fd
OBigVee <adedejivictor98@gmail.com>
2023-07-24 22:21:57 +0100

    task 0 done :heavy_check_mark:

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,9 +4,10 @@ a given employee ID,
 returns information about his/her Todo list progress.
 """
 
+import json
 import requests
 import sys
-import json
+
 
 if __name__ == "__main__":
     employee_id = sys.argv[1]
