e9cb2b31691bc7ff358382cfe1a00949201a636c
OBigVee <adedejivictor98@gmail.com>
2023-07-29 08:27:50 +0100

    task 3 completed :heavy_check_mark:

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3 
+#!/usr/bin/python3
 """
 Using task #0, extend Python script to export data in the JSON format.
 Requirements:
@@ -6,25 +6,25 @@ Requirements:
 Records all tasks from all employees
 Format must be:
 {
-    "USER_ID": [ 
+    "USER_ID": [
         {
-            "username": "USERNAME", 
+            "username": "USERNAME",
             "task": "TASK_TITLE",
             "completed": TASK_COMPLETED_STATUS
-        }, 
+        },
         {
             "username": "USERNAME",
             "task": "TASK_TITLE",
             "completed": TASK_COMPLETED_STATUS
         }
-        , 
+        ,
         ... ],
-        "USER_ID": [ 
+        "USER_ID": [
             {
                 "username": "USERNAME",
                 "task": "TASK_TITLE",
                 "completed": TASK_COMPLETED_STATUS
-            }, 
+            },
             {
                 "username": "USERNAME",
                 "task": "TASK_TITLE",
@@ -52,11 +52,11 @@ File name must be: todo_all_employees.json
 #         name = user_url[0]["name"]
 #         username = user_url[0]["username"]
 
-       
+
 #         for job_done in todo_list_url:
 #             if job_done["userId"] == id_arr[i]:
 #                 user_todo_list.append(job_done)
-     
+
 #     tasks_list = []
 #     # write to json
 #     with open(filename, "w", encoding="utf-8") as json_file:
@@ -74,37 +74,40 @@ File name must be: todo_all_employees.json
 
 #         data = {id_arr[i]: tasks_list for i in range(len(id_arr))}
 #         json.dump(data, json_file, indent=4)
-if __name__ == '__main__':
+if __name__ == "__main__":
     from requests import get
     from sys import argv
     import json
 
     #  Fetch todos and user
-    todos = get('https://jsonplaceholder.typicode.com/todos').json()
-    users = get('https://jsonplaceholder.typicode.com/users').json()
+    todos = get("https://jsonplaceholder.typicode.com/todos").json()
+    users = get("https://jsonplaceholder.typicode.com/users").json()
 
     #  Compose data to be dumped in JSON file
     data = {}
 
     for user in users:
         #  Extract user specific todos
-        userId = user['id']
+        userId = user["id"]
         user_todos = []
         for todo in todos:
-            if todo['userId'] == userId:
+            if todo["userId"] == userId:
                 user_todos.append(todo)
 
         #  Create user data object
         tasks = []
 
         for todo in user_todos:
-            obj = {"username": user['username'], "task": todo['title'],
-                   "completed": todo['completed']}
+            obj = {
+                "username": user["username"],
+                "task": todo["title"],
+                "completed": todo["completed"],
+            }
             tasks.append(obj)
 
         data.update({userId: tasks})
 
     #  write data to a JSON file
-    fname = 'todo_all_employees.json'
-    with open(fname, 'w', encoding='utf-8') as f:
+    fname = "todo_all_employees.json"
+    with open(fname, "w", encoding="utf-8") as f:
         json.dump(data, f)
