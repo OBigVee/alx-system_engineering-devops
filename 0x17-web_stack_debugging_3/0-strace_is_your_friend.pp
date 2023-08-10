6867026175d50036a268d3ec12bf59942b04151c
OBigVee <adedejivictor98@gmail.com>
2023-08-10 10:39:32 +0100

    task 0 completed :heavy_check_mark:

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,10 +2,7 @@
 
 exec { 'fix_typo_1':
   command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
-  path    => '/bin',
+  path    => '/bin:/usr/bin',
 }
 
-exec { 'fix_typo_2':
-  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
-  path    => '/usr/bin',
-}
+
