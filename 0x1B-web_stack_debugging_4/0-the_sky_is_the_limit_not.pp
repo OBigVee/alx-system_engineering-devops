f13e38021de4b0855516daae78cd6dc24b385934
OBigVee <adedejivictor98@gmail.com>
2023-08-19 15:47:22 +0100

    task 0 completed :heavy_check_mark:

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,13 @@
+# 0. Sky is the limit, let's bring that limit higher
+
+exec {'increase-Ulimit':
+    command => 'sed -i "s/15/4096" etc/default/nginx',
+    path => '/bin/:/usr/local/bin',
+    notify => Exec['restart-nginx']
+}
+
+exec {'restart-nginx':
+    command => 'nginx restart',
+    path => '/etc/init.d/',
+    refreshonly => true,
+}
