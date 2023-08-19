9727c771b2badad6db31ed4388ecd7355bf202a7
OBigVee <adedejivictor98@gmail.com>
2023-08-19 15:59:21 +0100

    task 0 completed :heavy_check_mark:

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,12 +2,12 @@
 
 exec {'increase-Ulimit':
     command => "sed -i 's/15/4096' /etc/default/nginx",
-    path => '/bin/:/usr/local/bin',
+    path => ['/bin/:/usr/local/bin'],
     notify => Exec['restart-nginx'],
 }
 
 exec {'restart-nginx':
     command => 'nginx restart',
-    path => '/etc/init.d/',
+    path => ['/etc/init.d/'],
     refreshonly => true,
 }
