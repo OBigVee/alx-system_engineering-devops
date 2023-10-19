6f11a4efed6c789f4fa7c50ef7bae468f5d6ea7b
obigvee <adedejivictor98@gmail.com>
2023-10-19 15:02:28 +0100

    web stack debuggin_4 completed

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,6 @@
+# change the OS configuration so that it is possible to login with the holberton user and open file without any error message.
+exec { 'change-os-configuration-for-holberton-user':
+  command => "bash -c \"sed -iE 's/^holberton hard nofile 5/holberton hard nofile 88888/' /etc/security/limits.conf; \
+                      sed -iE 's/^holberton soft nofile 4/holberton soft nofile 88888/' /etc/security/limits.conf\"",
+  path    => '/usr/bin:/usr/sbin:/bin'
+}
---
+++ 0x1B-web_stack_debugging_4/README.md
@@ -0,0 +1,5 @@
+# Webstack Debugging 4
+
+The nginx web-server isn't doing well under the pressure of huge incoming requests(2000), it is getting a large amount of failed requests.
+
+ApacheBench is used to benchmark and stimulate the HTTP requests to web-server nginx. 
